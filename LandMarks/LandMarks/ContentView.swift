b900a19b799310b89716b08425098ec25f3bc607
Gaurav Joshi <49354547+gjoshi2424@users.noreply.github.com>
2020-08-26 15:49:27 -0700

    Add files via upload

---
+++ LandMarks/LandMarks/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  LandMarks
+//
+//  Created by Gaurav Joshi on 7/8/20.
+//  Copyright © 2020 Gaurav Joshi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            MapView().frame(height: 300)
+                .edgesIgnoringSafeArea(.top)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title).foregroundColor(.black)
+                HStack {
+                    Text("Joshua Tree National Park").font(.subheadline)
+                    Spacer()
+                    Text("California").font(.subheadline)
+                }
+            }
+    .padding()
+        Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
